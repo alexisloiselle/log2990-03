0a29d34b9f924a81b24e25850fca13804899c2d2
Unknown <louiscormier2@gmail.com>
2018-01-24 08:57:06 -0500

    Remove some test that will be move in black-case-manager

--- server/app/Crossword/grid-manager.spec.ts
+++ server/app/Crossword/grid-manager.spec.ts
@@ -19,25 +19,13 @@ describe("Grid-Generator", () => {
     beforeEach(() => {
         
     });
-
+    
     describe("Grid Generation", () => {
         it("Should have the right width", () => {
             expect(cases.length).to.equal(height);
         });
         it("Should have the right height", () => {
             expect(cases[0].length).to.equal(width);
-        });
-    });
-
-    let array: [number, number][] = gridManager.populateArray();
-
-    describe("Array Population", () => {
-        it("Should have the right tuple", () => {
-            expect(array[0]).to.equal([0,0]);
-        });
-
-        it("Should have the right tuple", () => {
-            expect(array[9]).to.equal([1,1]);
         });
     });
 });
