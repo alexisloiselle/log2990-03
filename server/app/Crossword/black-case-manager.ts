4731ac2dbf9624e2d4f2515867ee2f825297e2a2
Louis Cormier <louiscormier2@gmail.com>
2018-01-24 08:05:46 -0500

    Create some functions for the Black Case Manager
    Create the functions findRandomCase, findCaseByPosition and removeFromArray

--- server/app/Crossword/black-case-manager.ts
+++ server/app/Crossword/black-case-manager.ts
@@ -21,12 +21,39 @@ export class BlackCaseManager {
         }
     }
 
-    public generateBlackCases(percent: number){
+    public findRandomCase(): [number, number] {
+        return this.possibleCase[Math.floor(Math.random() * (this.possibleCase.length - 1))];
+    }
+
+    public findCaseByPosition(position: [number, number]): number {
+        for (let i: number = 0; i < this.possibleCase.length; i++){
+            if (this.possibleCase[i] == position){
+                return i
+            }
+        }
+        return -1;
+    }
+
+    public removeFromArray(index: number): void {
+        this.possibleCase[index] = this.possibleCase[this.possibleCase.length - 1];
+        this.possibleCase.pop();
+    }
+
+    public generateBlackCases(percent: number) {
         let maxNumberOfCases = percent/100 * this.height * this.width;
         this.populateArray();
 
-        // for (let i: number = 0; i < maxNumberOfCase; i++) {
-        //     // TODO
-        // }
+        for (let i: number = 0; i < maxNumberOfCases; i++) {
+            let caseIsFound: boolean = false;
+            while (!caseIsFound && this.possibleCase.length > 0){
+                let position: [number, number] = this.findRandomCase();
+                let caseIsFound: boolean; // = Katherine's function
+                // TODO : Add Katherine's functions 
+                this.removeFromArray(this.findCaseByPosition(position)); //try catch si position = -1
+            }
+            if (this.possibleCase.length == 0){
+                break;
+            }
+        }
     }
 }
