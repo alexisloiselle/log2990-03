575882257f4bb855f1c0b553e790ec815bacad87
Katherine Nadeau <katherine.nadeau@polymtl.ca>
2018-01-30 11:35:34 -0500

    resolve conflict

--- server/app/Crossword/case.ts
+++
@@ -1,9 +0,0 @@
-export class Case {
-    private isBlack: boolean;
-    private letterTyped: string;
-    private rightLetter: string;
-
-    public setIsBlack(isBlack: boolean) {
-        this.isBlack = isBlack;
-    }
-}
