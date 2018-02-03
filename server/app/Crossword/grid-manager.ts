bc93913ded69d41e5ef37c8d50c0f9808eeb1227
Louis Cormier <louiscormier2@gmail.com>
2018-02-02 19:03:45 -0500

    Remove word-sorter and add sort to grid-manager
    Since the typescript sort function is already good, I decided to use it

--- server/app/Crossword/grid-manager.ts
+++ server/app/Crossword/grid-manager.ts
@@ -1,13 +1,18 @@
 import { Case } from "./case";
 import { BlackCasePlacer } from "./black-case-placer";
+import { Word } from "./word"
+import { GridScanner } from "./grid-scanner";
 
 export class GridManager {
 
     private grid: Case[][];
     private blackCasePlacer: BlackCasePlacer;
     private difficulty: string;
+    private words: Word[];
 
-    constructor() {}
+    constructor() {
+        this.words = [];
+    }
 
     public getGrid(): Case[][] {
         return this.grid;
@@ -28,11 +33,17 @@ export class GridManager {
     }
 
     public generateGrid(height: number, width: number, difficulty: string): void {
-        // TODO : create negative exception for generate grid
         this.createGrid(height, width);
+
         this.difficulty = difficulty;
+
         this.blackCasePlacer = new BlackCasePlacer(height, width);
         const percentage: number = 30;
         this.blackCasePlacer.generateBlackCases(this.grid, percentage);
+
+        let gridScanner: GridScanner = new GridScanner();
+        this.words = gridScanner.findWords(this.grid);
+        
+        this.words.sort((a, b)=>b.getLength()-a.getLength());
     }
 }
