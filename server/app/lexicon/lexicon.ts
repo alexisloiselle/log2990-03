8021b072f28ec71d015a9dac9d606f9a3047387a
Alexis Loiselle <alexis.loiselle@polymtl.ca>
2018-02-04 15:42:41 -0500

    AL: npm start working

--- server/app/lexicon/lexicon.ts
+++ server/app/lexicon/lexicon.ts
@@ -9,7 +9,6 @@ import {
     MAX_FREQ,
     USELESS_CHAR
 } from "../config";
-import { Response } from "express-serve-static-core";
 
 export class Lexicon {
 
@@ -25,7 +24,7 @@ export class Lexicon {
         const url: string = `${API_URL}${word}&${API_DEFS}`;
 
         return new Promise<string[]>((resolve: Function) => {
-            request(`${url}`, (error: Error, response: Response, body: Body) => {
+            request(url, (error: any, response: any, body: any) => {
                 body = JSON.parse(body);
                 definitions = body[0].defs.slice(0, MAX_DEFS).map((def: string) => {
                     return def.slice(USELESS_CHAR);
@@ -41,7 +40,7 @@ export class Lexicon {
         const url: string = `${API_URL}${word}&${API_FREQ}`;
 
         return new Promise<number>((resolve: Function) => {
-            request(`${url}`, (error: Error, response: Response, body: Body) => {
+            request(url, (error: any, response: any, body: any) => {
                 body = JSON.parse(body);
                 freq = body[0].tags[0].slice(USELESS_CHAR);
                 resolve(+freq);
--- server/app/routes/index.ts
+++ server/app/routes/index.ts
@@ -2,17 +2,19 @@ import { Request, Response, NextFunction } from "express";
 import { Message } from "../../../common/communication/message";
 import "reflect-metadata";
 import { injectable, } from "inversify";
+import { Lexicon } from "../lexicon/lexicon";
 
 module Route {
 
     @injectable()
     export class Index {
 
-        public helloWorld(req: Request, res: Response, next: NextFunction): void {
+        public async helloWorld(req: Request, res: Response, next: NextFunction): Promise<void> {
             const message: Message = new Message();
             message.title = "Hello";
-            message.body = "World";
-            res.send(JSON.stringify(message));
+            const def: string[] = await Lexicon.getDefinitions("world");
+            message.body = def[1];
+            res.send(`${message.title} ${message.body}`);
         }
     }
 }
