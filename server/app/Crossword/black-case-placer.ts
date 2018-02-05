b9d9a6c5fd6122ac3df0a3b4936f0cd536f41dc6
Katherine Nadeau <katherine.nadeau@polymtl.ca>
2018-02-05 00:05:30 -0500

    Word-placer in progress

--- server/app/Crossword/black-case-placer.ts
+++ server/app/Crossword/black-case-placer.ts
@@ -9,82 +9,82 @@ export class BlackCasePlacer {
         return column;
     }
 
-    public placeBlackCase(grid: Case[][], line: number, row: number): boolean {
-        // We place a black case if the row and line still have at least a word and
+    public placeBlackCase(grid: Case[][], line: number, column: number): boolean {
+        // We place a black case if the column and line still have at least a word and
         //  if we don't create 1 or 2 char words
-        if (this.tableHas1Word(this.getColum(grid, row), line) && this.tableHas1Word(grid[line], row)
-        && this.doesntCreateShortWords(grid, line, row)) {
-            this.updateCases(grid, line, row);
+        if (this.tableHas1Word(this.getColum(grid, column), line) && this.tableHas1Word(grid[line], column)
+        && this.doesntCreateShortWords(grid, line, column)) {
+            this.updateCases(grid, line, column);
             return true;
         }
         return false;
     }
 
-    public updateCases(grid: Case[][], line: number, row: number) {
+    private updateCases(grid: Case[][], line: number, column: number) {
         // Update newly created word cases info
-        let newWordLength = grid[line][row].getHorizontalWordLength() - grid[line][row].getHorizontalPositionInWord() - 1;
-        for (let i = 1; i <= grid[line][row].getHorizontalPositionInWord(); i++) {
-            grid[line][row - i].setHorizontalWordLength(grid[line][row].getHorizontalPositionInWord());
+        let newWordLength = grid[line][column].getHorizontalWordLength() - grid[line][column].getHorizontalPositionInWord() - 1;
+        for (let i = 1; i <= grid[line][column].getHorizontalPositionInWord(); i++) {
+            grid[line][column - i].setHorizontalWordLength(grid[line][column].getHorizontalPositionInWord());
         }
         for (let i = 0; i < newWordLength; i++) {
-            grid[line][row + i + 1].setHorizontalPositionInWord(i);
-            grid[line][row + i + 1].setHorizontalWordLength(newWordLength);
+            grid[line][column + i + 1].setHorizontalPositionInWord(i);
+            grid[line][column + i + 1].setHorizontalWordLength(newWordLength);
         }
 
-        newWordLength = grid[line][row].getVerticalWordLength() - grid[line][row].getVerticalPositionInWord() - 1;
-        for (let j = 1; j <= grid[line][row].getVerticalPositionInWord(); j++) {
-            grid[line - j][row].setVerticalWordLength(grid[line][row].getVerticalPositionInWord());
+        newWordLength = grid[line][column].getVerticalWordLength() - grid[line][column].getVerticalPositionInWord() - 1;
+        for (let j = 1; j <= grid[line][column].getVerticalPositionInWord(); j++) {
+            grid[line - j][column].setVerticalWordLength(grid[line][column].getVerticalPositionInWord());
         }
         for (let j = 0; j < newWordLength; j++) {
-            grid[line + j + 1][row].setVerticalPositionInWord(j);
-            grid[line + j + 1][row].setVerticalWordLength(newWordLength);
+            grid[line + j + 1][column].setVerticalPositionInWord(j);
+            grid[line + j + 1][column].setVerticalWordLength(newWordLength);
         }
 
         // Update the new black case info
-        grid[line][row].setIsBlack(true);
-        grid[line][row].setHorizontalPositionInWord(0);
-        grid[line][row].setHorizontalWordLength(0);
-        grid[line][row].setVerticalPositionInWord(0);
-        grid[line][row].setVerticalWordLength(0);
+        grid[line][column].setIsBlack(true);
+        grid[line][column].setHorizontalPositionInWord(0);
+        grid[line][column].setHorizontalWordLength(0);
+        grid[line][column].setVerticalPositionInWord(0);
+        grid[line][column].setVerticalWordLength(0);
     }
 
     // **Currently doesnt allow 2 letters word but could be changed !
-    public doesntCreateShortWords(grid: Case[][], line: number, row: number): boolean {
+    private doesntCreateShortWords(grid: Case[][], line: number, column: number): boolean {
         // Newly created horizontal word length if we place a black case
-        const leftWordLength = grid[line][row].getHorizontalPositionInWord();
-        const rightWordLength = grid[line][row].getHorizontalWordLength() - grid[line][row].getHorizontalPositionInWord() - 1;
+        const leftWordLength = grid[line][column].getHorizontalPositionInWord();
+        const rightWordLength = grid[line][column].getHorizontalWordLength() - grid[line][column].getHorizontalPositionInWord() - 1;
 
         // Making sure that if we create 1 or 2 char horizontal words, they can still form 3+ char word vertically
         if (leftWordLength == 1 || leftWordLength == 2) {
             for (let i = 1; i <= leftWordLength; i++) {
-                if (grid[line][row - i].getVerticalWordLength() < 3) {
+                if (grid[line][column - i].getVerticalWordLength() < 3) {
                     return false;
                 }
             }
         }
         if (rightWordLength == 1 || rightWordLength == 2) {
             for (let i = 1; i <= rightWordLength; i++) {
-                if (grid[line][row + i].getVerticalWordLength() < 3) {
+                if (grid[line][column + i].getVerticalWordLength() < 3) {
                     return false;
                 }
             }
         }
 
         // Newly created vertical word length if we place a black case
-        const upperWordLength = grid[line][row].getVerticalPositionInWord();
-        const lowerWordLength = grid[line][row].getVerticalWordLength() - grid[line][row].getVerticalPositionInWord() - 1;
+        const upperWordLength = grid[line][column].getVerticalPositionInWord();
+        const lowerWordLength = grid[line][column].getVerticalWordLength() - grid[line][column].getVerticalPositionInWord() - 1;
 
         // Making sure that if we create 1 or 2 char vertical words, they can still form 3+ char word horizontally
         if (upperWordLength == 1 || upperWordLength == 2) {
             for (let i = 1; i <= upperWordLength; i++) {
-                if (grid[line - i][row].getHorizontalWordLength() < 3) {
+                if (grid[line - i][column].getHorizontalWordLength() < 3) {
                     return false;
                 }
             }
         }
         if (lowerWordLength == 1 || lowerWordLength == 2) {
             for (let i = 1; i <= lowerWordLength; i++) {
-                if (grid[line + i][row].getHorizontalWordLength() < 3) {
+                if (grid[line + i][column].getHorizontalWordLength() < 3) {
                     return false;
                 }
             }
@@ -93,7 +93,7 @@ export class BlackCasePlacer {
         return true;
     }
 
-    public tableHas1Word(table: Case[], pos: number): boolean {
+    private tableHas1Word(table: Case[], pos: number): boolean {
         let nbWhiteCase = 0;
 
         // Looping on the table to make sure there's at least one word of more than 2 char
