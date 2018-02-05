2d0a49a8d748e6a4f3d92f393e9423494a6619c2
Louis Cormier <louiscormier2@gmail.com>
2018-02-05 10:32:45 -0500

    Grid Scanner tests + bug fixed

--- server/app/Crossword/grid-scanner.ts
+++ server/app/Crossword/grid-scanner.ts
@@ -9,11 +9,12 @@ export class GridScanner {
         for (let line: number = 0; line < grid.length; line++) {
             for (let column: number = 0; column < grid[0].length; column++) {
                 if (grid[line][column].getHorizontalPositionInWord() == 0 &&
-                grid[line][column].getHorizontalWordLength() > 2) {
+                grid[line][column].getHorizontalWordLength() > 2 
+                && !grid[line][column].getIsBlack()) {
                     words.push(new Word(line, column, grid[line][column].getHorizontalWordLength(), true));
                 }
                 if (grid[line][column].getVerticalPositionInWord() == 0 &&
-                grid[line][column].getVerticalWordLength() > 2) {
+                grid[line][column].getVerticalWordLength() > 2 && !grid[line][column].getIsBlack()) {
                     words.push(new Word(line, column, grid[line][column].getVerticalWordLength(), false));
                 }
             }
