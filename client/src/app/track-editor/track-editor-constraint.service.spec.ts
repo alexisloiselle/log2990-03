1eac347eededc6bc1a2b71b4dea69af7281ac3bf
Jeremie Miglierina <jeremie.miglierina@polymtl.ca>
2018-02-05 13:44:27 -0500

    New Test track-editor-constraint.service.spec

--- client/src/app/track-editor/track-editor-constraint.service.spec.ts
+++ client/src/app/track-editor/track-editor-constraint.service.spec.ts
@@ -8,14 +8,23 @@ import { Vector } from './vector/vector';
 describe("Track-Editor-Constraint", () => {
 
   const pointStart: PointCoordinates = new PointCoordinates(2, 6);
+  const pointParallel: PointCoordinates = new PointCoordinates(2, 6);
   const pointEnd: PointCoordinates = new PointCoordinates (1, 3);
   const pointLess45: PointCoordinates = new PointCoordinates(1, 2);
   const vectorTest: Vector = new Vector(pointStart, pointEnd);
   const vectorTestMoins45: Vector = new Vector(pointStart, pointLess45);
-  
+  const vectorParallel: Vector = new Vector(pointStart, pointEnd);
+
   it("Angle should be less than 45 degree", () => {
-    expect((vectorTest.calculateAngle(vectorTestMoins45).toFixed(3))).toBe(((4.4 * Math.PI)/180).toFixed(3));
+    expect(vectorTest.calculateAngle(vectorTestMoins45).toFixed(3)).toBe(((4.4 * Math.PI)/180).toFixed(3));
+  });
+  
+  it("Angle shouldn't be parallel", () => {
+    expect(vectorTest.isParallel(vectorTestMoins45)).toBeFalsy;
   });
 
+  it("Angle shouldn be parallel", () => {
+    expect(vectorTest.isParallel(vectorParallel)).toBeTruthy;
+  });
 
 });
