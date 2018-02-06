5eca2d4278f59cf8e1d29c0467fc300160476b20
Louis Cormier <louiscormier2@gmail.com>
2018-02-05 19:45:55 -0500

    Refactoring Grid Manager
    Moved Grid  Manager to grid generator.
    Added a new grid manager that has the ownership of the grid.

--- server/app/Crossword/grid-manager.ts
+++ server/app/Crossword/grid-manager.ts
@@ -1,41 +1,14 @@
 import { Case } from "./case";
-import { BlankGridCreator } from "./blank-grid-creator";
-import { BlackCaseGenerator } from "./black-case-generator";
-import { Word } from "./word";
-import { GridScanner } from "./grid-scanner";
-import { WordPlacer } from "./word-placer";
+import { GridGenerator } from "./grid-generator";
 
 export class GridManager {
 
     private grid: Case[][];
-    // private difficulty: string;
-    private words: Word[];
 
-    constructor() {
-        this.words = [];
-    }
-
-    public getGrid(): Case[][] {
-        return this.grid;
-    }
-
-    public generateGrid(height: number, width: number, difficulty: string): void {
-        const blankGridCreator: BlankGridCreator = new BlankGridCreator();
-        this.grid = blankGridCreator.createGrid(height, width);
-
-        // this.difficulty = difficulty;
-
-        const blackCaseGenerator: BlackCaseGenerator = new BlackCaseGenerator(height, width);
-        const percentage: number = 50;
-        blackCaseGenerator.generateBlackCases(this.grid, percentage);
-
-        const gridScanner: GridScanner = new GridScanner();
-        this.words = gridScanner.findWords(this.grid);
-
-        this.words.sort((a: Word, b: Word) => b.getLength() - a.getLength());
-        const wordPlacer: WordPlacer = new WordPlacer();
-        wordPlacer.identifyConstraint(this.grid);
-        wordPlacer.fitWord(this.grid, this.words, 0);
-
+    // This class has the ownership of the grid and will have the responsability to manage it
+    constructor(difficulty: string) {
+        const gridManager: GridGenerator = new GridGenerator();
+        const dimension: number = 10;
+        this.grid = gridManager.generateGrid(dimension, dimension);
     }
 }
