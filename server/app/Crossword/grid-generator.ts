87fd658931a403742985fafb09706a6a157f39d7
Katherine Nadeau <katherine.nadeau@polymtl.ca>
2018-02-17 16:16:54 -0500

    Test for definition adder (in progress)

--- server/app/Crossword/grid-generator.ts
+++ server/app/Crossword/grid-generator.ts
@@ -12,21 +12,17 @@ export class GridGenerator {
     public static async generateGrid(height: number, width: number, difficulty: string): Promise<Grid> {
         let words: Word[] = [];
 
-        // tslint:disable-next-line:max-func-body-length
-        return new Promise<Grid>((resolve: Function) => {
+        return new Promise<Grid>(async (resolve: Function) => {
             let grid: Case[][];
-
             const PERCENTAGE: number = 38;
 
             const blankGridCreator: BlankGridCreator = new BlankGridCreator();
             grid = blankGridCreator.createGrid(height, width);
-
             const blackCaseGenerator: BlackCaseGenerator = new BlackCaseGenerator(height, width);
             blackCaseGenerator.generateBlackCases(grid, PERCENTAGE);
 
             words = GridScanner.findWords(grid);
             GridScanner.identifyConstraint(grid, words);
-
             words.sort((a: Word, b: Word) => b.NbConstraints - a.NbConstraints);
             const constraintsQueue: Word[] = [];
             constraintsQueue.push(words[0]);
@@ -38,20 +34,7 @@ export class GridGenerator {
             const definitionAdder: DefinitionAdder = new DefinitionAdder();
             words = GridScanner.findWords(grid);
             definitionAdder.addWords(grid, words);
-            definitionAdder.addDefinitions(words, difficulty);
-
-            //#region alexis
-            for (const row of grid) {
-                for (const position of row) {
-                    if (position.IsBlack) {
-                        process.stdout.write("#");
-                    } else {
-                        process.stdout.write(position.RightLetter);
-                    }
-                }
-                process.stdout.write("\n");
-            }
-            //#endregion
+            await definitionAdder.addDefinitions(words, difficulty);
 
             const finalGrid: Grid = new Grid(grid, words);
             resolve(finalGrid);
