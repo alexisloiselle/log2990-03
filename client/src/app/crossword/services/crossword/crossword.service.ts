4d9a6d4b2da9bdeb8d5cf8214af9782bdaf2978a
Alexis Loiselle <alexis.loiselle@polymtl.ca>
2018-02-20 11:47:28 -0500

    AL: formatted grid interface

--- client/src/app/crossword/services/crossword/crossword.service.ts
+++ client/src/app/crossword/services/crossword/crossword.service.ts
@@ -1,13 +1,16 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+import { FormattedGrid } from "../../formatted-grid";
+import "rxjs/add/operator/map";
+import { Observable } from "rxjs/Observable";
 
 @Injectable()
 export class CrosswordService {
 
     public constructor(private http: HttpClient) {}
 
-    public generateGrid() {
-        return this.http.get("http://localhost:3000/crossword-generator/generate-grid");
+    public generateGrid(): Observable<FormattedGrid> {
+        return this.http.get("http://localhost:3000/crossword-generator/generate-grid")
+            .map(res => res.json() as FormattedGrid);
     }
-
 }
