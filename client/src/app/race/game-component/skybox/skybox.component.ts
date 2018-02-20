85ff88252949d85d6886aafeec5076bd521b841d
Jeremie Miglierina <jeremie.miglierina@polymtl.ca>
2018-02-19 19:03:04 -0500

    Skybox renderer

--- client/src/app/race/game-component/skybox/skybox.component.ts
+++ client/src/app/race/game-component/skybox/skybox.component.ts
@@ -2,24 +2,34 @@ import { Component, OnInit } from "@angular/core";
 import * as THREE from "three";
 
 
+
 @Component({
     selector: "app-skybox",
     templateUrl: "./skybox.component.html",
-    styleUrls: ["./skybox.component.css"]
+    styleUrls: ["./skybox.component.css"],
+    //providers:[RenderService],
 })
 export class SkyboxComponent implements OnInit {
 
     // private container = document.createElement('div');
-    // private camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 100000);
+    private camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 100000);
     private scene = new THREE.Scene();
     // private stats // 
     private material: THREE.ShaderMaterial;
+    private renderer = new THREE.WebGLRenderer({antialias: true});
+    private container = document.createElement( 'div' );
+    
 
     constructor() {
         this.createScene();
      }
 
-    ngOnInit() { }
+    ngOnInit() { 
+      this.loadCubeTexture();
+      this.initShader();
+      this.initRenderer();
+
+    }
 
     public loadCubeTexture(): THREE.Texture {
         let urlPrefix = "./skybox/pictures/";
@@ -43,9 +53,17 @@ export class SkyboxComponent implements OnInit {
         this.material.needsUpdate = true;
     }
     public createScene() {
-        this.initShader();
         const skyboxMesh = new THREE.Mesh(new THREE.CubeGeometry(100000, 100000, 100000), this.material);
         this.material.needsUpdate = true;
         this.scene.add(skyboxMesh);
     }
+    public initRenderer() {
+
+	  this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.container.appendChild( this.renderer.domElement );
+    document.body.appendChild(this.container ); // not sure about that
+    this.renderer.render(this.scene,this.camera);
+    }
+
+    
 }
