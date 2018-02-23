f5d52a8cb10a7305e25e42b6a5fb0b6b06902f84
Vincent Dandenault <vdandenault@gmail.com>
2018-02-23 11:43:34 -0500

    Kawel

--- server/app/routes/authRoute.ts
+++ server/app/routes/authRoute.ts
@@ -2,16 +2,17 @@ import { Request, Response, NextFunction } from "express";
 import { injectable, } from "inversify";
 
 const MONGO_URL = "mongodb://lesChats:ChatonsRoux123@ds233218.mlab.com:33218/log2990-03-db";
+var MongoClient = require('mongodb').MongoClient;
 
 
 module Route {
-
     @injectable()
     export class AuthRoute {
     
     public auth(req: Request, res: Response, next: NextFunction) {
-        require("mongodb").MongoClient.connect(MONGO_URL, function (err: any, db: any) {
-            const collection = db.getCollection("admin"); 
+        MongoClient.connect(MONGO_URL, function (err: any, db: any) {
+            console.log(db);
+            const collection = db.collection("admin"); 
             collection.findOne({ id: "password" }, function (findErr: any, doc: any) {
                 const isPassOk: boolean = req.body.password === doc.value; 
                 res.send(JSON.stringify(isPassOk));
@@ -20,7 +21,7 @@ module Route {
         });
     }
     public changePassword(req: Request, res: Response, next: NextFunction) {
-    require("mongodb").MongoClient.connect(MONGO_URL, function (err: any, db: any) {
+        MongoClient.connect(MONGO_URL, function (err: any, db: any) {
         const collection = db.collection("admin");
         collection.updateOne({ id: "password" }, { $set: { value: req.body.newPassword } }, null,
             function (updateErr: any, updateDb: any) {
