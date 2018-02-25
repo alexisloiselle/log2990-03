1d7e09885de9e6104b5b913ce75d90595ed0817a
Louis Cormier <louiscormier2@gmail.com>
2018-02-25 13:57:03 -0500

    add mocks to crossword-service and auth-service tests

--- client/src/app/crossword/services/crossword/crossword.service.spec.ts
+++ client/src/app/crossword/services/crossword/crossword.service.spec.ts
@@ -1,13 +1,16 @@
 import { TestBed, inject } from "@angular/core/testing";
+import { XHRBackend } from "@angular/http";
 
 import { CrosswordService } from "./crossword.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { MockBackend } from "@angular/http/testing";
 
 describe("CrosswordService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [CrosswordService]
+      providers: [CrosswordService,
+                  { provide: XHRBackend, useClass: MockBackend }]
     });
   });
 
