4beee2505ea4e668c3cfac896a61878302a61bad
Louis Cormier <louiscormier2@gmail.com>
2018-02-27 14:39:33 -0500

    Create multiplayer view

---
+++ client/src/app/crossword/multiplayer/multiplayer-game/multiplayer-game.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { MultiplayerGameComponent } from './multiplayer-game.component';
+
+describe('MultiplayerGameComponent', () => {
+  let component: MultiplayerGameComponent;
+  let fixture: ComponentFixture<MultiplayerGameComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ MultiplayerGameComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(MultiplayerGameComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
