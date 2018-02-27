1d87164d546a80237604b49c39955d6ff2dca18d
Louis <louiscormier2@gmail.com>
2018-02-26 21:24:49 -0500

    fix unit test for should create in multiplayer-menu

--- client/src/app/crossword/multiplayer-menu/multiplayer-menu.component.spec.ts
+++ client/src/app/crossword/multiplayer-menu/multiplayer-menu.component.spec.ts
@@ -1,6 +1,7 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { MultiplayerMenuComponent } from "./multiplayer-menu.component";
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe("MultiplayerMenuComponent", () => {
     let component: MultiplayerMenuComponent;
@@ -8,6 +9,7 @@ describe("MultiplayerMenuComponent", () => {
 
     beforeEach(async(() => {
         TestBed.configureTestingModule({
+            imports: [RouterTestingModule],
             declarations: [MultiplayerMenuComponent]
         })
             .compileComponents();
