2a7391ffe504ba3d9bea05cdf8165a38672f8177
Louis <louiscormier2@gmail.com>
2018-02-26 23:04:27 -0500

    Unique game name verifier

--- client/src/app/crossword/create-game/create-game.component.ts
+++ client/src/app/crossword/create-game/create-game.component.ts
@@ -9,10 +9,15 @@ export class CreateGameComponent implements OnInit {
 
     public difficulty: string;
     public gameName: string;
+    public nameAlreadyUsed: boolean;
+
+    public test: string;
 
     public constructor() {
         this.gameName = "";
         this.difficulty = "";
+        this.nameAlreadyUsed = false;
+        this.test = "I love Mimi";
     }
 
     public ngOnInit(): void {
@@ -20,10 +25,11 @@ export class CreateGameComponent implements OnInit {
 
     public updateGameName(event: any): void {
         this.gameName = event.target.value;
+        this.nameAlreadyUsed = (this.gameName === this.test);
     }
 
     public isFormValid(): boolean {
-        return (this.gameName.length !== 0 && this.difficulty !== "");
+        return (this.gameName.length !== 0 && this.difficulty !== "" && !this.nameAlreadyUsed);
     }
 
     public setDifficulty(difficulty: string): void {
