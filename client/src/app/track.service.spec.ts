c18a3ffdc033246624359cf7142d3401f3ed573c
Katherine Nadeau <katherine.nadeau@polymtl.ca>
2018-02-26 22:59:57 -0500

    Tests in track service

--- client/src/app/track.service.spec.ts
+++ client/src/app/track.service.spec.ts
@@ -1,14 +1,15 @@
 import { TestBed, inject } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
 
 import { TrackService } from "./track.service";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
-import { RaceTrack } from "./race/raceTrack";
+import { RaceTrack, RaceType } from "./race/raceTrack";
+import { PointCoordinates } from "./race/track-editor/canvas/point-coordinates";
 
 describe("TrackService", () => {
     beforeEach(() => {
         TestBed.configureTestingModule({
             imports: [HttpClientTestingModule],
-            providers: [TrackService,
+            providers: [TrackService]
         });
     });
 
@@ -16,10 +17,34 @@ describe("TrackService", () => {
         expect(service).toBeTruthy();
     }));
 
-    describe("getTracks()", () => {
-        it("should return ", inject([TrackService], async (service: TrackService) => {
-            const tracks: RaceTrack[] = await service.getTracks();
-            expect(tracks.length).toBeGreaterThan(0);
-        }));
-    });
+    it("getTracks() should return an array of length > 0", inject([TrackService], async (service: TrackService) => {
+        const tracks: RaceTrack[] = await service.getTracks();
+        expect(tracks.length).toBeGreaterThan(0);
+    }));
+
+    it("addTrack() should return true", inject([TrackService], async (service: TrackService) => {
+        const points: PointCoordinates[] = [];
+        const track: RaceTrack = new RaceTrack("lol", "test", RaceType.Amateur, points);
+        const added: boolean = await service.addTrack(track);
+        expect(added).toEqual(true);
+    }));
+
+    it("updateTrack() should return true", inject([TrackService], async (service: TrackService) => {
+        const points: PointCoordinates[] = [];
+        const track: RaceTrack = new RaceTrack("lol", "test", RaceType.Amateur, points);
+        const updated: boolean = await service.updateTrack("0", track);
+        expect(updated).toEqual(true);
+    }));
+
+    it("updateTrack() should return true", inject([TrackService], async (service: TrackService) => {
+        const points: PointCoordinates[] = [];
+        const track: RaceTrack = new RaceTrack("lol", "test", RaceType.Amateur, points);
+        const updated: boolean = await service.updateTrack("0", track);
+        expect(updated).toEqual(true);
+    }));
+
+    it("deleteTrack() should return true", inject([TrackService], async (service: TrackService) => {
+        const updated: boolean = await service.deleteTrack("0");
+        expect(updated).toEqual(true);
+    }));
 });
