a3456b012463f1b612a5750628e780d2e9cad773
Alexis Loiselle <alexis.loiselle@polymtl.ca>
2018-02-28 11:56:18 -0500

    4 spaces

--- client/src/app/homepage/homepage.component.ts
+++ client/src/app/homepage/homepage.component.ts
@@ -1,14 +1,14 @@
 import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: "app-homepage",
-  templateUrl: "./homepage.component.html",
-  styleUrls: ["./homepage.component.css"]
+    selector: "app-homepage",
+    templateUrl: "./homepage.component.html",
+    styleUrls: ["./homepage.component.css"]
 })
 export class HomepageComponent implements OnInit {
 
-  public constructor() { }
+    public constructor() { }
 
-  public ngOnInit(): void {
-  }
+    public ngOnInit(): void {
+    }
 }
