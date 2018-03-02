e927a7ea7d1f185ae68f5b912e17710edf31b010
Vincent Dandenault <vdandenault@gmail.com>
2018-03-02 09:39:54 -0500

    Render-track

---
+++ client/src/app/render-track.service.spec.ts
@@ -0,0 +1,15 @@
+import { TestBed, inject } from '@angular/core/testing';
+
+import { RenderTrackService } from './render-track.service';
+
+describe('RenderTrackService', () => {
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [RenderTrackService]
+    });
+  });
+
+  it('should be created', inject([RenderTrackService], (service: RenderTrackService) => {
+    expect(service).toBeTruthy();
+  }));
+});
