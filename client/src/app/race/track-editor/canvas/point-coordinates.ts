896355122a29fc71e5928f6fcc4e9abbb2ed2d1d
Unknown <mtigrini@gmail.com>
2018-03-06 12:08:23 -0500

    Track generation works now in the game

--- client/src/app/race/track-editor/canvas/point-coordinates.ts
+++ client/src/app/race/track-editor/canvas/point-coordinates.ts
@@ -20,6 +20,15 @@ export class PointCoordinates {
                 this.Y <= otherPoint.Y + MINIMUM_DISTANCE_BETWEEN_POINTS);
     }
 
+    public getAngle(otherPoint: PointCoordinates): number {
+        return Math.atan((otherPoint.Y - this.Y)/(otherPoint.X - this.X));
+    }
+
+    public getDistance(otherPoint: PointCoordinates) : number {
+        return Math.sqrt(Math.pow(this.X-otherPoint.X, 2) + 
+                         Math.pow(this.Y-otherPoint.Y, 2) );
+    }
+
     public get X(): number {
         return this.x;
     }
