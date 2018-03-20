4a20b992ec0f440e116fbe4e288cfdd1e6c0f410
Louis Cormier <louiscormier2@gmail.com>
2018-03-20 09:19:43 -0400

    LINT on Socket Service

--- client/src/app/crossword/services/socket.service.ts
+++ client/src/app/crossword/services/socket.service.ts
@@ -1,14 +1,14 @@
-import { Injectable } from "@angular/core"; 
-import * as io from "socket.io-client"; 
- 
-@Injectable() 
-export class SocketService { 
- 
-    private socket: SocketIOClient.Socket; 
- 
-    public constructor() {} 
- 
-    public connect(): void { 
+import { Injectable } from "@angular/core";
+import * as io from "socket.io-client";
+
+@Injectable()
+export class SocketService {
+
+    private socket: SocketIOClient.Socket;
+
+    public constructor() {}
+
+    public connect(): void {
         this.socket = io("http://localhost:3000/"); 
-    } 
-} 
+    }
+}
