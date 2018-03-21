dfc9ae649312c0a4367420fdad66b53e9c1bb3f0
Alexis Loiselle <alexis.loiselle@polymtl.ca>
2018-03-21 14:15:58 -0400

    tests for camera done

--- client/src/app/render-service/camera.service.spec.ts
+++ client/src/app/render-service/camera.service.spec.ts
@@ -10,6 +10,8 @@ import { RenderTrackService } from "../render-track/render-track.service";
 const INITIAL_HEIGHT_ORTHO: number = 90;
 const INITIAL_Z_THIRDPERSON: number = 10;
 const INITIAL_HEIGHT_THIRDPERSON: number = 4;
+const ZOOM_LIMIT: number = 2;
+const UNZOOM_LIMIT: number = 0.6;
 
 describe("CameraService", () => {
     let cameraService: CameraService;
@@ -41,13 +43,13 @@ describe("CameraService", () => {
     }));
 
     it("orthographic should be at right position", inject([CameraService], (service: CameraService) => {
-        expect(cameraService.orthographicCamera.position.x).toEqual(renderService.car.position.x);
-        expect(cameraService.orthographicCamera.position.z).toEqual(renderService.car.position.z);
+        expect(cameraService.orthographicCamera.position.x).toEqual(renderService.car.Position.x);
+        expect(cameraService.orthographicCamera.position.z).toEqual(renderService.car.Position.z);
         expect(cameraService.orthographicCamera.position.y).toEqual(INITIAL_HEIGHT_ORTHO);
     }));
 
     it("thirdPerson should be at right position", inject([CameraService], (service: CameraService) => {
-        expect(cameraService.thirdPersonCamera.position.x).toEqual(renderService.car.position.x);
+        expect(cameraService.thirdPersonCamera.position.x).toEqual(renderService.car.Position.x);
         expect(cameraService.thirdPersonCamera.position.y).toEqual(INITIAL_HEIGHT_THIRDPERSON);
         expect(cameraService.thirdPersonCamera.position.z).toEqual(INITIAL_Z_THIRDPERSON);
     }));
@@ -67,6 +69,10 @@ describe("CameraService", () => {
     }));
 
     it("third person camera should follow car", inject([CameraService], (service: CameraService) => {
+        expect(cameraService.IsOrthographicEnabled).toEqual(true);
+        cameraService.switchView(renderService.car);
+        expect(cameraService.IsOrthographicEnabled).toEqual(false);
+
         expect(cameraService.thirdPersonCamera.position.x).toEqual(renderService.car.Position.x);
         expect(cameraService.thirdPersonCamera.position.y - INITIAL_HEIGHT_THIRDPERSON).toEqual(renderService.car.Position.y);
         expect(cameraService.thirdPersonCamera.position.z - INITIAL_Z_THIRDPERSON).toEqual(renderService.car.Position.z);
@@ -77,5 +83,33 @@ describe("CameraService", () => {
         expect(cameraService.thirdPersonCamera.position.x).toEqual(renderService.car.Position.x);
         expect(cameraService.thirdPersonCamera.position.y - INITIAL_HEIGHT_THIRDPERSON).toEqual(renderService.car.Position.y);
         expect(cameraService.thirdPersonCamera.position.z - INITIAL_Z_THIRDPERSON).toEqual(renderService.car.Position.z);
+    }));
+
+    it("zoom should work", inject([CameraService], (service: CameraService) => {
+        expect(cameraService.IsOrthographicEnabled).toEqual(true);
+
+        const initialZoom: number = cameraService.orthographicCamera.zoom;
+        cameraService.zoom(true);
+        expect(cameraService.orthographicCamera.zoom).toBeGreaterThan(initialZoom);
+    }));
+
+    it("unzoom should work", inject([CameraService], (service: CameraService) => {
+        expect(cameraService.IsOrthographicEnabled).toEqual(true);
+
+        const initialZoom: number = cameraService.orthographicCamera.zoom;
+        cameraService.zoom(false);
+        expect(cameraService.orthographicCamera.zoom).toBeLessThan(initialZoom);
+    }));
+
+    it("shouldn't be outside of limits", inject([CameraService], (service: CameraService) => {
+        expect(cameraService.IsOrthographicEnabled).toEqual(true);
+
+        cameraService.orthographicCamera.zoom = ZOOM_LIMIT;
+        cameraService.zoom(true);
+        expect(cameraService.orthographicCamera.zoom).toEqual(ZOOM_LIMIT);
+
+        cameraService.orthographicCamera.zoom = UNZOOM_LIMIT;
+        cameraService.zoom(false);
+        expect(cameraService.orthographicCamera.zoom).toEqual(UNZOOM_LIMIT);
     }));
 });
