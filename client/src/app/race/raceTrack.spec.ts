47774a084487f99665a287f8bc89985a6f1b6214
Unknown <katherine.nadeau@polymtl.ca>
2018-03-26 14:37:09 -0400

    Useless code removed

--- client/src/app/race/raceTrack.spec.ts
+++
@@ -1,4 +0,0 @@
-/* tslint:disable:no-magic-numbers */
-describe("RaceTrack", () => {
-
-});
--- client/src/app/race/raceTrack.ts
+++ client/src/app/race/raceTrack.ts
@@ -5,16 +5,10 @@ export class RaceTrack {
     public id: string;
     public name: string;
     public description: string;
-    public lapNumber: number;
     public type: RaceType;
-    public ratings: number[] = [];
-    public timesPlayed: number = 0;
-    public times: TrackTime[] = [];
-    public bestTime: TrackTime;
     public points: THREE.Vector2[] = [];
     public segments: THREE.LineCurve[] = [];
     public width: number = 20;
-    public center: THREE.Vector2;
 
     public constructor(name: string, description: string, type: RaceType, points: THREE.Vector2[]) {
         this.name = name;
@@ -36,13 +30,6 @@ export class RaceTrack {
     public normalise(vector: THREE.Vector2, origin: THREE.Vector2): THREE.Vector2 {
         return new THREE.Vector2((vector.x - origin.x), (vector.y - origin.y));
     }
-}
-
-export class TrackTime {
-    public constructor(
-        public time: number,
-        public name: string,
-    ) {}
 }
 
 export enum RaceType {
