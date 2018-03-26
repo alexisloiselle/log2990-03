9a6357319f544f476673ba166a9fcd2e6d17fe8a
Unknown <mtigrini@gmail.com>
2018-03-26 14:53:23 -0400

    Changed constant name for a more relevant one

--- client/src/app/render-track/render-track.service.spec.ts
+++ client/src/app/render-track/render-track.service.spec.ts
@@ -2,7 +2,7 @@ import { TestBed, inject } from "@angular/core/testing";
 
 import { RenderTrackService } from "./render-track.service";
 
-const FOUR: number = 4;
+const NUMBEROFCARS: number = 4;
 
 describe("RenderTrackService", () => {
   beforeEach(() => {
@@ -23,18 +23,18 @@ describe("RenderTrackService", () => {
     const positionNumbers3: Array<number> = renderTrackService.generateRandomCarPositions();
 
     // We first check if the size of the arrays is correct
-    if (positionNumbers1.length !== FOUR || positionNumbers2.length !== FOUR || positionNumbers3.length !== FOUR) {
+    if (positionNumbers1.length !== NUMBEROFCARS || positionNumbers2.length !== NUMBEROFCARS || positionNumbers3.length !== NUMBEROFCARS) {
       positionNumbersOk = false;
     }
     // Then we check if the numbers are in the correct range
     for (const n of positionNumbers1) {
-      if (n > FOUR || n < 1) { positionNumbersOk = false; break; }
+      if (n > NUMBEROFCARS || n < 1) { positionNumbersOk = false; break; }
     }
     for (const n of positionNumbers2) {
-      if (n > FOUR || n < 1) { positionNumbersOk = false; break; }
+      if (n > NUMBEROFCARS || n < 1) { positionNumbersOk = false; break; }
     }
     for (const n of positionNumbers3) {
-      if (n > FOUR || n < 1) { positionNumbersOk = false; break; }
+      if (n > NUMBEROFCARS || n < 1) { positionNumbersOk = false; break; }
     }
     // Then we check if the function really generates aleatory numbers
     // With 3 arrays, there's almost no chance that the numbers will be the same
