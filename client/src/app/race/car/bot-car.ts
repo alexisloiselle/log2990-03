69cc2e4a9794ac6d5933c38f91b476c5b354997d
Unknown <katherine.nadeau@polymtl.ca>
2018-03-26 11:45:57 -0400

    perfectionnement virtual cars

--- client/src/app/race/car/bot-car.ts
+++ client/src/app/race/car/bot-car.ts
@@ -3,43 +3,39 @@ import { LineCurve, Vector2 } from "three";
 
 export class BotCar extends Car {
 
+    public MAX_SPEED: number = 65;
+
     public getPosition(): Vector2 {
         return new Vector2(this.mesh.position.z, this.mesh.position.x);
     }
 
-    public ajustDirection(trackSegment: LineCurve): void {
+    public ajustDirection(trackSegment: LineCurve, accelerate: boolean): void {
         const segmentDirection: Vector2 = new Vector2((trackSegment.v2.x - trackSegment.v1.x),
                                                       (trackSegment.v2.y - trackSegment.v1.y));
-        const angle: number = this.getPositiveAngle(this.direction.x, this.direction.z) -
-                              this.getPositiveAngle(segmentDirection.y, segmentDirection.x);
+        const angle: number = this.getPositiveAngle(new Vector2(this.direction.z, this.direction.x)) -
+                              this.getPositiveAngle(new Vector2(segmentDirection.x, segmentDirection.y));
 
-        if ((angle > 0 && this.isSmallestAngle(angle)) || (angle < 0 && !this.isSmallestAngle(angle))) {
-            this.turnRight();
+        if (this.speed.length() < this.MAX_SPEED) {
+            this.isAcceleratorPressed = accelerate && this.randomAcceleration();
+        }
+
+        if ((angle > 0 && Math.abs(angle) < Math.PI) || (angle < 0 && !(Math.abs(angle) < Math.PI))) {
+            this.steerRight();
         } else {
-            this.turnLeft();
+            this.steerLeft();
         }
     }
 
-    public turnLeft(): void {
-        this.isAcceleratorPressed = true;
-        this.steerLeft();
+    public randomAcceleration(): boolean {
+        return Math.random() >= (1 / 2);
     }
 
-    public turnRight(): void {
-        this.isAcceleratorPressed = true;
-        this.steerRight();
-    }
-
-    public getPositiveAngle(y: number, x: number): number {
-        let angle: number = Math.atan2(y, x);
+    public getPositiveAngle(vector: Vector2): number {
+        let angle: number = Math.atan2(vector.y, vector.x);
         if (angle < 0) {
             angle += Math.PI * 2;
         }
 
         return angle;
-    }
-
-    public isSmallestAngle(angle: number): boolean {
-        return Math.abs(angle) < Math.PI;
     }
 }
