39131943f7c8e0d58f6ac3a569782bb51d3fa426
Unknown <katherine.nadeau@polymtl.ca>
2018-03-26 16:20:50 -0400

    Last test fix

--- client/src/app/race/game-component/track-list-game/track-list-game.component.spec.ts
+++ client/src/app/race/game-component/track-list-game/track-list-game.component.spec.ts
@@ -1,25 +1,42 @@
 import { async, ComponentFixture, TestBed } from "@angular/core/testing";
-
 import { TrackListGameComponent } from "./track-list-game.component";
+import { RenderService } from "../../../render-service/render.service";
+import { CarEventHandlerService } from "../../../render-service/car-event-handler.service";
+import { CameraService } from "../../../render-service/camera.service";
+import { SkyboxService } from "../../../render-service/skybox.service";
+import { CollisionService } from "../../collisions/collision.service";
+import { TrackService } from "../../../track.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { RouterTestingModule } from "@angular/router/testing";
+import { RenderTrackService } from "../../../render-track/render-track.service";
 
 describe("TrackListGameComponent", () => {
-  let component: TrackListGameComponent;
-  let fixture: ComponentFixture<TrackListGameComponent>;
+    let component: TrackListGameComponent;
+    let fixture: ComponentFixture<TrackListGameComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ TrackListGameComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            imports: [HttpClientTestingModule, RouterTestingModule],
+            declarations: [TrackListGameComponent],
+            providers: [
+                TrackService,
+                RenderService,
+                CarEventHandlerService,
+                CameraService,
+                SkyboxService,
+                CollisionService,
+                RenderTrackService
+            ]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(TrackListGameComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(TrackListGameComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it("should create", () => {
-    expect(component).toBeTruthy();
-  });
+    it("should create", () => {
+        expect(component).toBeTruthy();
+    });
 });
