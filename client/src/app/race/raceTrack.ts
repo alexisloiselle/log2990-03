1646a31c10f0f74bde5f99d5b7a09cd0ab4bb95d
Jeremie Miglierina <jeremie.miglierina@polymtl.ca>
2018-04-06 13:12:51 -0400

    New BestTimes class in RaceTrack

--- client/src/app/race/raceTrack.ts
+++ client/src/app/race/raceTrack.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+const NUMBER_OF_BEST_TIMES_IN_ARRAY: number = 5;
 
 export class RaceTrack {
 
@@ -9,9 +10,11 @@ export class RaceTrack {
     public points: THREE.Vector2[] = [];
     public segments: THREE.LineCurve[] = [];
     public width: number = 20;
+    private bestTimes: BestTime;
 
     public constructor(name: string, description: string, type: RaceType, points: THREE.Vector2[]) {
         this.name = name;
+        this.bestTimes = new BestTime;
         this.description = description;
         this.type = type;
         for (const point of points) {
@@ -22,16 +25,47 @@ export class RaceTrack {
 
     public findSegments(): void {
         for (let i: number = 0; i < this.points.length - 1; i++) {
-            this.segments.push(new THREE.LineCurve(this.normalise(this.points[i], this.points[0]),
-                                                   this.normalise(this.points[i + 1], this.points[0])));
+            this.segments.push(new THREE.LineCurve(this.normalize(this.points[i], this.points[0]),
+                                                   this.normalize(this.points[i + 1], this.points[0])));
         }
     }
 
-    public normalise(vector: THREE.Vector2, origin: THREE.Vector2): THREE.Vector2 {
+    public normalize(vector: THREE.Vector2, origin: THREE.Vector2): THREE.Vector2 {
         return new THREE.Vector2((vector.x - origin.x), (vector.y - origin.y));
     }
 }
 
+export class BestTime {
+
+    private arrayBestTimes: Array<Player>;
+    private title: string;
+
+    public constructor() {
+        this.title = "Top 5 des meilleurs temps";
+        for (let i: number = 0; i < NUMBER_OF_BEST_TIMES_IN_ARRAY; i++) {
+            this.arrayBestTimes.push({time: 0, name: "N/a" });
+        }
+
+    }
+
+    public updateBestTime(newPlayer: Player): void {
+        for (let i: number = 0; i < NUMBER_OF_BEST_TIMES_IN_ARRAY; i++) {
+            if (newPlayer.time < this.arrayBestTimes[i].time) {
+                this.arrayBestTimes[i].time = newPlayer.time;
+                break;
+            }
+        }
+    }
+
+    public appendArrayBestTimes(): void {
+
+    }
+
+}
+export interface Player {
+    name: string;
+    time: number;
+}
 export enum RaceType {
     Amateur,
     Professional
