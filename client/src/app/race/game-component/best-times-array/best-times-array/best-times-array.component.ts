1ee7e7b62572ea6f16af276b44962051da2b4647
Alexis Loiselle <alexis.loiselle@polymtl.ca>
2018-04-09 20:01:30 -0400

    resolve conflicts

--- client/src/app/race/game-component/best-times-array/best-times-array/best-times-array.component.ts
+++ client/src/app/race/game-component/best-times-array/best-times-array/best-times-array.component.ts
@@ -1,63 +1,20 @@
-<<<<<<< HEAD
 import { Component, OnInit } from "@angular/core";
 import { BestTime } from "../../../raceTrack";
 
-@Component({
-  selector: "app-best-times-array",
-  templateUrl: "./best-times-array.component.html",
-  styleUrls: ["./best-times-array.component.css"]
-=======
-import { Component, OnInit } from '@angular/core';
-import { BestTime } from '../../../raceTrack';
-
-@Component({
-  selector: 'app-best-times-array',
-  templateUrl: './best-times-array.component.html',
-  styleUrls: ['./best-times-array.component.css']
->>>>>>> 0bf018c60fa3da05274842924e717834dab50967
-})
-
 const NUMBER_OF_BEST_TIMES_IN_ARRAY: number = 5;
 
-<<<<<<< HEAD
-export class BestTimeComponent {
+@Component({
+    selector: "app-best-times-array",
+    templateUrl: "./best-times-array.component.html",
+    styleUrls: ["./best-times-array.component.css"]
+})
+export class BestTimeComponent implements OnInit {
 
-  public bestTimes: BestTime;
+    public bestTimes: BestTime;
 
-  constructor() {
-    
-  }
+    public constructor() {
 
-  }
-
-}
-=======
-export class BestTime {
-
-  public arrayBestTimes: Array<Player>;
-  public title: string;
-
-  public constructor() {
-    this.title = "Top 5 des meilleurs temps";
-    for (let i: number = 0; i < NUMBER_OF_BEST_TIMES_IN_ARRAY; i++) {
-      this.arrayBestTimes.push({ time: 0, name: "N/a" });
     }
 
-  }
-
-  public updateBestTime(newPlayer: Player): void {
-    for (let i: number = 0; i < NUMBER_OF_BEST_TIMES_IN_ARRAY; i++) {
-      if (newPlayer.time < this.arrayBestTimes[i].time) {
-        this.arrayBestTimes.splice(i, 0, newPlayer);
-        this.arrayBestTimes.pop();
-        break;
-      }
-    }
-  }
-
+    public ngOnInit(): void {}
 }
-export interface Player {
-  name: string;
-  time: number;
-}
->>>>>>> 0bf018c60fa3da05274842924e717834dab50967
