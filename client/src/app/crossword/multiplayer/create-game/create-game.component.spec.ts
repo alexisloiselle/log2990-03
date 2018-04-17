2d9242a7ea721600c69a507b26f7e0cae5f420f0
Louis Cormier <louiscormier2@gmail.com>
2018-04-17 13:04:00 -0400

    All test for create game component

--- client/src/app/crossword/multiplayer/create-game/create-game.component.spec.ts
+++ client/src/app/crossword/multiplayer/create-game/create-game.component.spec.ts
@@ -7,6 +7,7 @@ import { HttpClientModule } from "@angular/common/http";
 import { SocketService } from "../../services/socket.service";
 
 describe("CreateGameComponent", () => {
+    // tslint:disable:no-any
     let component: CreateGameComponent;
     let fixture: ComponentFixture<CreateGameComponent>;
 
@@ -14,7 +15,8 @@ describe("CreateGameComponent", () => {
         TestBed.configureTestingModule({
             imports: [RouterTestingModule, HttpClientModule],
             declarations: [CreateGameComponent],
-            providers: [CrosswordService, SocketService]
+            providers: [CrosswordService,
+                        SocketService]
         })
             .compileComponents();
     }));
@@ -53,5 +55,78 @@ describe("CreateGameComponent", () => {
         expect(component.gameName).toEqual(newGameName);
     });
 
-    // TODO: createGame tu dois mock socketService, crosswordService et router comme jai expliquer dans join game
+    it("should not be a valid form if there is no game name", () => {
+        component.gameName = "";
+        component.username = "username";
+        component.difficulty = "test";
+        component.isCreateGameAlreadyClicked = false;
+        expect(component.isFormValid()).toBeFalsy();
+    });
+
+    it("should not be a valid form if there is no username", () => {
+        component.gameName = "gameName";
+        component.username = "";
+        component.difficulty = "test";
+        component.isCreateGameAlreadyClicked = false;
+        expect(component.isFormValid()).toBeFalsy();
+    });
+
+    it("should not be a valid form if there is no game difficulty", () => {
+        component.gameName = "gameName";
+        component.username = "username";
+        component.difficulty = "";
+        component.isCreateGameAlreadyClicked = false;
+        expect(component.isFormValid()).toBeFalsy();
+    });
+
+    it("should not be a valid form if the create button has been already clicked", () => {
+        component.gameName = "gameName";
+        component.username = "username";
+        component.difficulty = "test";
+        component.isCreateGameAlreadyClicked = true;
+        expect(component.isFormValid()).toBeFalsy();
+    });
+
+    it("should be a valid form if there is a game name, a username, a difficulty and it hasn't been clicked yet", () => {
+        component.gameName = "gameName";
+        component.username = "username";
+        component.difficulty = "test";
+        component.isCreateGameAlreadyClicked = false;
+        expect(component.isFormValid()).toBeTruthy();
+    });
+
+    const router: any = {
+        navigate: (url: [string, string, boolean]) => {}
+    };
+
+    const crosswordServise: any = {
+        isNameAlreadyUsed: async (gameName: string): Promise<boolean> => {
+            if (gameName === "alreadyUsed") {
+                return true;
+            } else {
+                return false;
+            }
+        },
+        createGame: async (gameName: string) => {}
+    };
+
+    const socketService: any = {
+        connect: () => {},
+        newGame: (gameName: string, username: string) => {}
+    };
+
+    const newComponent: CreateGameComponent = new CreateGameComponent(router, crosswordServise, socketService);
+    newComponent.ngOnInit();
+
+    it("should be able to click on create game when name is already used", async () => {
+        newComponent.gameName = "alreadyUsed";
+        await newComponent.createGame();
+        expect(newComponent.isCreateGameAlreadyClicked).toEqual(false);
+    });
+
+    it("should be able to click on create game again when name is not already used", async () => {
+        newComponent.gameName = "isNotAlreadyUsed";
+        await newComponent.createGame();
+        expect(newComponent.isCreateGameAlreadyClicked).toEqual(true);
+    });
 });
