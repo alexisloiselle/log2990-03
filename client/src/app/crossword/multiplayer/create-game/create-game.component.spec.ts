6b88074ac54618571b9d1af71c54f2136a97d1d1
Louis Cormier <louiscormier2@gmail.com>
2018-04-17 10:58:17 -0400

    Test for updateusername and updategamename in create game

--- client/src/app/crossword/multiplayer/create-game/create-game.component.spec.ts
+++ client/src/app/crossword/multiplayer/create-game/create-game.component.spec.ts
@@ -29,6 +29,30 @@ describe("CreateGameComponent", () => {
         expect(component).toBeTruthy();
     });
 
+    it("should update username when there is a name update", () => {
+        const newUsername: string = "new username";
+        // tslint:disable-next-line:no-any
+        const event: any = {
+            target: {
+                value: newUsername
+            }
+        };
+        component.updateUsername(event);
+        expect(component.username).toEqual(newUsername);
+    });
+
+    it("should update game name when there is a game name update", () => {
+        const newGameName: string = "new game name";
+        // tslint:disable-next-line:no-any
+        const event: any = {
+            target: {
+                value: newGameName
+            }
+        };
+        component.updateGameName(event);
+        expect(component.gameName).toEqual(newGameName);
+    });
+
     // TODO: comme jai dit pour le join game, mock l'event
     // TODO: createGame tu dois mock socketService, crosswordService et router comme jai expliquer dans join game
 });
