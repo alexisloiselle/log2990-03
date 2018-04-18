4688c4a402e16f6bdee48d221a992f5ae7a03bbe
Louis Cormier <louiscormier2@gmail.com>
2018-04-18 15:04:10 -0400

    Added test about loops and setpointcoordinates

--- client/src/app/race/track-editor/canvas/track-editor-model.spec.ts
+++ client/src/app/race/track-editor/canvas/track-editor-model.spec.ts
@@ -5,7 +5,30 @@ import { } from "jasmine";
 
 describe("CanvasComponent", () => {
     // tslint:disable:no-magic-numbers
-    const trackEditor: TrackEditorModel = new TrackEditorModel();
+    let trackEditor: TrackEditorModel;
+    beforeEach(() => {
+        trackEditor = new TrackEditorModel();
+    });
+
+    it ("should return false if a loop is not closed", () => {
+        trackEditor.addPoint(new Vector2(0, 0));
+        trackEditor.addPoint(new Vector2(15, 15));
+        expect(trackEditor.isLoopClosed()).toBeFalsy();
+    });
+
+    it ("should return false if there is less than or equal at 2", () => {
+        trackEditor.addPoint(new Vector2(0, 0));
+        trackEditor.addPoint(new Vector2(0, 0));
+        expect(trackEditor.isLoopClosed()).toBeFalsy();
+    });
+
+    it ("should return true if a loop is closed", () => {
+        trackEditor.addPoint(new Vector2(0, 0));
+        trackEditor.addPoint(new Vector2(15, 15));
+        trackEditor.addPoint(new Vector2(0, 15));
+        trackEditor.addPoint(new Vector2(0, 0));
+        expect(trackEditor.isLoopClosed()).toBeTruthy();
+    });
 
     it ("should return the vector at the given index", () => {
         const vector: Vector2 = new Vector2(1, 1);
@@ -20,5 +43,37 @@ describe("CanvasComponent", () => {
         const resultVector: Vector2 = trackEditor.getSinglePoint(-5);
         const expectedVector: Vector2 = new Vector2(-1, -1);
         expect(resultVector).toEqual(expectedVector);
+    });
+
+    it ("should return the error vector if index is higher than the number of points", () => {
+        const vector: Vector2 = new Vector2(1, 1);
+        trackEditor.addPoint(vector);
+        const resultVector: Vector2 = trackEditor.getSinglePoint(5);
+        const expectedVector: Vector2 = new Vector2(-1, -1);
+        expect(resultVector).toEqual(expectedVector);
+    });
+
+    it ("should set the points for the vector at the given index", () => {
+        const vector: Vector2 = new Vector2(1, 1);
+        trackEditor.addPoint(vector);
+        const mouseCoordinates: Vector2 = new Vector2(3, 3);
+        trackEditor.setPointCoordinates(0, mouseCoordinates);
+        expect(trackEditor.getSinglePoint(0)).toEqual(mouseCoordinates);
+    });
+
+    it("should not change the coordinates if the index is negative", () => {
+        const vector: Vector2 = new Vector2(1, 1);
+        trackEditor.addPoint(vector);
+        const mouseCoordinates: Vector2 = new Vector2(3, 3);
+        trackEditor.setPointCoordinates(-5, mouseCoordinates);
+        expect(trackEditor.getSinglePoint(0)).toEqual(vector);
+    });
+
+    it("should not change the coordinates if the index is out of bound", () => {
+        const vector: Vector2 = new Vector2(1, 1);
+        trackEditor.addPoint(vector);
+        const mouseCoordinates: Vector2 = new Vector2(3, 3);
+        trackEditor.setPointCoordinates(5, mouseCoordinates);
+        expect(trackEditor.getSinglePoint(0)).toEqual(vector);
     });
 });
