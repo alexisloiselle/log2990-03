30666fe00e05aaec42647449f169ff67a0a02c5b
Vincent Dandenault <vdandenault@gmail.com>
2018-04-18 15:50:19 -0400

    testsDone!

--- client/src/app/crossword/grid/focus-case.service.spec.ts
+++ client/src/app/crossword/grid/focus-case.service.spec.ts
@@ -1,15 +1,18 @@
-import { TestBed, inject } from '@angular/core/testing';
+import { TestBed, inject } from "@angular/core/testing";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { FocusCaseService } from "./focus-case.service";
+import { DefinitionService} from "../services/crossword/definition.service";
+import {CrosswordService} from "../services/crossword/crossword.service";
 
-import { FocusCaseService } from './focus-case.service';
-
-describe('FocusCaseService', () => {
+describe("FocusCaseService", () => {
   beforeEach(() => {
     TestBed.configureTestingModule({
-      providers: [FocusCaseService]
+      imports: [HttpClientTestingModule],
+      providers: [FocusCaseService, DefinitionService, CrosswordService]
     });
   });
 
-  it('should be created', inject([FocusCaseService], (service: FocusCaseService) => {
+  it("should be created", inject([FocusCaseService], (service: FocusCaseService) => {
     expect(service).toBeTruthy();
   }));
 });
