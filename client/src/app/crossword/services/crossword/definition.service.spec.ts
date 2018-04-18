3cd5d4ffd20256fe4f96631940290dd7834aa7a2
Vincent Dandenault <vdandenault@gmail.com>
2018-04-18 12:12:31 -0400

    hotFix!/definition.service.specs

--- client/src/app/crossword/services/crossword/definition.service.spec.ts
+++ client/src/app/crossword/services/crossword/definition.service.spec.ts
@@ -3,6 +3,7 @@ import { TestBed, inject } from "@angular/core/testing";
 import { DefinitionService } from "./definition.service";
 import { HttpClientTestingModule } from "@angular/common/http/testing";
 import { CrosswordService } from "./crossword.service";
+import {Word} from "../../word";
 
 describe("DefinitionService", () => {
     beforeEach(() => {
@@ -17,5 +18,11 @@ describe("DefinitionService", () => {
 
     it("should be created", inject([DefinitionService], (service: DefinitionService) => {
         expect(service).toBeTruthy();
+    }));
+
+    it("should attribut the selected word", inject([DefinitionService], (service: DefinitionService) => {
+        const word = new Word("mot", "leMot", false, 0, 0); 
+        service.handleClickDef(word);
+        expect(service.SelectedWord == word).toBeTruthy();
     }));
 });
